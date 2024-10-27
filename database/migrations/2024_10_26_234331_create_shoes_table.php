9aa5f1ab2a8d9834f0b65efa1b2b0787ea6fadde
rijaluddina <rijaluddin.alazmi@outlook.com>
2024-10-27 07:06:31 +0700

    make:model & migrations

---
+++ database/migrations/2024_10_26_234331_create_shoes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shoes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->string('thumbnail');
+            $table->text('about');
+            $table->unsignedBigInteger('price');
+            $table->unsignedBigInteger('stock');
+            $table->boolean('is_popuar');
+            $table->foreignId('category_id')->constrained()->onDelete();
+            $table->foreignId('brand_id')->constrained()->onDelete();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shoes');
+    }
+};
